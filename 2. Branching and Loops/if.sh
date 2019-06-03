87f592309dc50a454b7ea44debe745231f98ace6
pisey <pisey@marinax.biz>
2019-06-03 13:56:02 +0700

    add else and else if

--- 2. Branching and Loops/if.sh
+++ 2. Branching and Loops/if.sh
@@ -4,6 +4,8 @@ COLOR=$1
 if [ $COLOR == "Blue" ]
  then
     echo "The color is blue"
+ else
+     echo "The color is Not blue"
 fi
 
 USER_GUESS=$2
@@ -11,4 +13,9 @@ COMPUTER=50
 if [ $USER_GUESS -lt $COMPUTER ]
  then
    echo "You're too low"
+ elif [ $USER_GUESS -gt $COMPUTER ]
+  then
+    echo "You're highter"
+ else
+    echo "You're guess"
 fi
