ffddaad819f51608c7637c12096728e41a66747f
pisey <pisey@marinax.biz>
2019-06-03 13:50:51 +0700

    add if

---
+++ 2. Branching and Loops/if.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+COLOR=$1
+if [ $COLOR == "Blue" ]
+ then
+    echo "The color is blue"
+fi
+
+USER_GUESS=$2
+COMPUTER=50
+if [ $USER_GUESS -lt $COMPUTER ]
+ then
+   echo "You're too low"
+fi
